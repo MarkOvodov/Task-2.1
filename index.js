d74fa5cb9faccd957a42a3845069b3126c33a180
MarkOvodov <ovodovmark13@gmail.com>
2025-03-21 18:12:45 +0300

    добавил forEach и стрелочные функции

--- index.js
+++ index.js
@@ -5,42 +5,31 @@ const search = 'История';
 
 // console.log(catalog);
 
-function allLanguagesInCatalog() {
-    let listOfLanguages = []
-    for (let i = 0; i < catalog.length; i++) {
-        if (listOfLanguages.includes(catalog[i].language) === false) {
-            listOfLanguages.push(catalog[i].language)
-        }
+let listOfLanguages = []
+catalog.forEach((elem) => {
+    if (listOfLanguages.includes(elem.language) === false) {
+        listOfLanguages.push(elem.language)
     }
-    return listOfLanguages
-}
+})
 
-// console.log(allLanguagesInCatalog())
+console.log(listOfLanguages)
 
-function allBooksByCentury(century) {
-    let listOfBooks = []
-    for (let i = 0; i < catalog.length; i++) {
-        if (catalog[i].year > ((century - 1) * 100) && catalog[i].year < ((century) * 100)) {
-            listOfBooks.push(catalog[i].title)
-        }
-    }
-    return listOfBooks
-}
+let listOfBooks = []
+catalog.forEach((elem) => {
+    if (elem.year > ((century - 1) * 100) && elem.year < ((century) * 100))
+        listOfBooks.push(elem.title)
+})
 
-// console.log(allBooksByCentury(century))
+console.log(listOfBooks)
 
-function searchByAuthorOrTitle(search) {
-    let result = []
-    for (let i = 0; i < catalog.length; i++) {
-        let author = catalog[i].author
-        let title = catalog[i].title
-        if (author.includes(search) === true || author.includes(search.toLowerCase()) === true) {
-            result.push(`${catalog[i].author} - ${catalog[i].title}`)
-        } else if (title.includes(search) === true || title.includes(search.toLowerCase()) === true) {
-            result.push(`${catalog[i].author} - ${catalog[i].title}`)
-        } 
-    }
-    return result
-}
+
+let result = []
+catalog.forEach(function (elem) {
+    if (elem.author.includes(search) === true || elem.author.includes(search.toLowerCase()) === true) 
+        result.push(`${elem.author} - ${elem.title}`)
+    else if (elem.title.includes(search) === true || elem.title.includes(search.toLowerCase()) === true)
+        result.push(`${elem.author} - ${elem.title}`)
+})
+
 // Функция принимает только название ИЛИ только автора
-console.log(searchByAuthorOrTitle(search))
+console.log(result)
